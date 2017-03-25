fd4c9f7ca171d71f7bbddc43a1b42e08d3e27d7d
Prafull Bhosale <prbhosal@microsoft.com>
2017-03-25 00:20:31 -0700

    Fix startup for scaffolding.

--- Startup.cs
+++ Startup.cs
@@ -7,25 +7,36 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
+using Microsoft.EntityFrameworkCore;
+using MvcMovie.Models;
 
 namespace MyMvc
 {
     public class Startup
     {
-       
+        public Startup(IHostingEnvironment env)
+        {
+            var builder = new ConfigurationBuilder()
+                .SetBasePath(env.ContentRootPath)
+                .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true);
+                //.AddJsonFile($"appsettings.{env.EnvironmentName}.json", optional: true);
+            Configuration = builder.Build();
+        }
 
+        public IConfigurationRoot Configuration {get;}
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
             // Add framework services.
             services.AddMvc();
+
+            services.AddDbContext<MvcMovieContext>(options =>
+                    options.UseSqlServer(Configuration.GetConnectionString("MvcMovieContext")));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app)
-        {           
-
-
+        {
             app.UseStaticFiles();
 
             app.UseMvc(routes =>
