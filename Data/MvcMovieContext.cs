8d78715e3efea7b9b6f4b099c032614215fa1764
Rick Anderson <riande@microsoft.com>
2017-03-24 17:43:19 -1000

    init

---
+++ Data/MvcMovieContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace MvcMovie.Models
+{
+    public class MvcMovieContext : DbContext
+    {
+        public MvcMovieContext (DbContextOptions<MvcMovieContext> options)
+            : base(options)
+        {
+        }
+
+        //public MvcMovieContext()
+        //{
+        //}
+
+        public DbSet<MvcMovie.Models.Movie> Movie { get; set; }
+    }
+}
---
+++ Data/MvcMovieContextFactory.cs
@@ -0,0 +1,18 @@
+﻿
+//using Microsoft.EntityFrameworkCore;
+//using Microsoft.EntityFrameworkCore.Infrastructure;
+//using MvcMovie.Models;
+
+//namespace MyProject
+//{
+//    public class MvcMovieContextFactory : IDbContextFactory<MvcMovieContext>
+//    {
+//        public MvcMovieContext Create()
+//        {
+//            var optionsBuilder = new DbContextOptionsBuilder<MvcMovieContext>();
+//            optionsBuilder.UseSqlite("Data Source=blog.db");
+
+//            return new MvcMovieContext(optionsBuilder.Options);
+//        }
+//    }
+//}
